42be03c1364afd6bdca7ffe337a64bef941775ce
sarthakghimire <sarthakghimire.sg@gmail.com>
2025-07-21 10:57:33 +0545

    exception handled

--- src/api/fetchPosts.js
+++ src/api/fetchPosts.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
 export const fetchPosts = async () => {
-  const response = await axios.get("/nepali_social_posts_with_comments.json");
-  return response.data;
+  try {
+    const response = await axios.get("/nepali_social_posts_with_comments.json");
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    throw error;
+  }
 };
