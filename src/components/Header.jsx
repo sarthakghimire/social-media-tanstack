291a1fecf6ea44a107c429508c7b4818161034d1
sarthakghimire <sarthakghimire.sg@gmail.com>
2025-07-21 16:18:13 +0545

    smol changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="bg-blue-600 text-white py-4 shadow-md">
+    <header className="bg-blue-600 text-white cursor-pointer hover:text-amber-100 py-4 shadow-md">
       <div className="container mx-auto text-center text-xl font-semibold">
         Nepali Social Feed
       </div>
